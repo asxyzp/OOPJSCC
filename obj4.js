c3d30231185a3e080f015659929f98b2dafa07c0
asxyzp <asxyzp@gmail.com>
2020-09-15 23:13:07 +0530

    Understanding error w/ cyclic prototypal inheritance

--- obj4.js
+++ obj4.js
@@ -15,6 +15,9 @@ let user = {
 console.log(admin.__proto__, user.__proto__);
 
 //user doesn't has admin CRUD priviliges
-user.__proto__=
+user.__proto__=admin;
+
+//Uncomment the statement below & execute the statement below to see cyclic prototypal error
+//admin.__proto__=user;
 console.log(admin.__proto__, user.__proto__);
 console.log(user.userID,user.userID,user.hasCRUDPrivileges);
